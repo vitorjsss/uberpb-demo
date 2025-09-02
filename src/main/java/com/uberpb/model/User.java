a2b5d9bb75e27efebfc508d6c5e25498bf2f7b0c
vitorjsss <vitorjsssmamede.1@gmail.com>
2025-09-01 23:00:52 -0300

    Primeiro commit: projeto Maven UberPB

---
+++ src/main/java/com/uberpb/model/User.java
@@ -0,0 +1,49 @@
+package com.uberpb.model;
+
+public class User {
+    private String nome;
+    private String email;
+    private String cpf;
+    private String senha;
+    private String tipo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+}
